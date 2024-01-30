ba9d62372df0442581117f977d6c30d48a622bc1
Adrian <AdrianCCollier@gmail.com>
2024-01-30 10:24:03 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import { Routes, Route } from 'react-router-dom'
 import Topbar from './scenes/global/Topbar'
 import Sidebar from './scenes/global/Sidebar'
 import Dashboard from './scenes/dashboard'
-// import Invoices from './scenes/invoices'
-// import Contacts from './scenes/contacts'
-// import Bar from './scenes/bar'
-// import Form from './scenes/form'
-// import Line from './scenes/line'
-// import Pie from './scenes/pie'
-// import FAQ from './scenes/faq'
-// import Geography from './scenes/geography'
+
+import Bar from './scenes/bar'
+import Form from './scenes/form'
+import Line from './scenes/line'
+import Pie from './scenes/pie'
+import FAQ from './scenes/faq'
+import Geography from './scenes/geography'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { ColorModeContext, useMode } from './theme'
 // import Calendar from './scenes/calendar/calendar'
 
 function App() {
-  const [theme, colorMode] = useMode();
+  const [theme, colorMode] = useMode()
 
   return (
     <ColorModeContext.Provider value={colorMode}>
