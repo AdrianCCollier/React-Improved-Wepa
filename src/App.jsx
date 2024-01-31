ee8709d8294c3a2033f6ab539ab5509240724a0b
Adrian <AdrianCCollier@gmail.com>
2024-01-31 11:25:24 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
+              
               
               
               
