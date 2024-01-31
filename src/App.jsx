5da424db2b187c0a6296fa04e4db771b490011bd
Adrian <AdrianCCollier@gmail.com>
2024-01-31 11:25:59 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
               
               
               
-              <Route path="/geography" element={<Geography />} />
+            
             </Routes>
           </main>
         </div>
