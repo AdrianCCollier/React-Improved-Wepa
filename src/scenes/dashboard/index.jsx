6e180f6f35530a2586442b65326b5f6a232b73f2
Adrian <AdrianCCollier@gmail.com>
2024-02-01 15:45:19 -0700

    Update index.jsx

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -15,7 +15,7 @@ import PersonAddIcon from '@mui/icons-material/PersonAdd'
 import TrafficIcon from '@mui/icons-material/Traffic'
 import Header from '../../components/Header'
 
-// import GeographyChart from '../../components/GeographyChart'
+
 
 import BarChart from '../../components/BarChart'
 import StatBox from '../../components/StatBox'
