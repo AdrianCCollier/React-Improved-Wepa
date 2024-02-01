0a129392408a5c639ecfb1159624d6a32137f183
Adrian <AdrianCCollier@gmail.com>
2024-02-01 15:46:35 -0700

    Update index.jsx

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -12,7 +12,7 @@ import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined'
 import EmailIcon from '@mui/icons-material/Email'
 import PointOfSaleIcon from '@mui/icons-material/PointOfSale'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
-import TrafficIcon from '@mui/icons-material/Traffic'
+
 import Header from '../../components/Header'
 import StatBox from '../../components/StatBox'
 
